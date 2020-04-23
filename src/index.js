3067be81eabb474d0092bf28e05493806773717f
Jeong <inyeop0212@gmail.com>
2020-04-23 20:53:13 +0900

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDom from 'react-dom';
+import React from 'react';
+import App from './components/App/App';
+import './index.css';
+
+ReactDom.render(<App />, document.getElementById('root'));
