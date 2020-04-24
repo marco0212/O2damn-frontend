aa1a41602a2970870c20ee076e70730b71fb982c
Jeong <inyeop0212@gmail.com>
2020-04-24 15:09:11 +0900

    Render Lobby #1

--- src/scenes/Lobby/Lobby.js
+++ src/scenes/Lobby/Lobby.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export default function Lobby() {
-  return <div>Lobby</div>;
+  return (
+    <>
+      <h1>O2 Damn</h1>
+      <p>online, web-based rhythm game</p>
+      <button>Enter Game</button>
+    </>
+  );
 }
