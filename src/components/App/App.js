53ff821033a319125a31fc4c420ed148c3f458a6
Jeong <inyeop0212@gmail.com>
2020-04-24 15:02:38 +0900

    Create Scenes and Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import Lobby from '../../scenes/Lobby/Lobby';
 
-export default function App () {
-  return <div>App</div>;
+export default function App ({ status }) {
+  switch (status) {
+    default:
+      return <Lobby />;
+  }
 }
