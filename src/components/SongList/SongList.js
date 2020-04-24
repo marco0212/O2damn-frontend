5c4c24a9e5504d3d5838ab458929b9b576ccfa53
Jeong <inyeop0212@gmail.com>
2020-04-25 00:38:25 +0900

    Render MusicCollection #2

--- src/components/SongList/SongList.js
+++ src/components/SongList/SongList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SongItem from '../SongItem/SongItem';
 import styled from 'styled-components';
 
 export default function({ songs, currentSongId, onSongItemClick }) {
@@ -14,18 +15,12 @@ export default function({ songs, currentSongId, onSongItemClick }) {
           const isActive = currentSongId === id;
           return (
             <SongItem
-              key={id}
-              onClick={onSongItemClick.bind(null, id)}
+              id={id}
+              title={title}
+              artistName={artistName}
               isActive={isActive}
-            >
-              <div>
-                <h4>{title}</h4>
-                <strong>{artistName}</strong>
-              </div>
-              {
-                isActive && <button>Start</button>
-              }
-            </SongItem>
+              onSongItemClick={onSongItemClick}
+            />
           );
         })
       }
@@ -36,27 +31,4 @@ export default function({ songs, currentSongId, onSongItemClick }) {
 const SongList = styled.ul`
   flex: 1;
   overflow: scroll;
-`;
-const SongItem = styled.li`
-  border-radius: 5px;
-  background-color: #fff;
-  padding: 15px;
-  margin-bottom: 21px;
-  display: flex;
-  justify-content: space-between;
-  ${props => props.isActive && 'background-color: #323232;color: #fff;'}
-
-  &:last-child {
-    margin-bottom: 0;
-  }
-
-  & > div {
-    flex: 1;
-  }
-
-  & h4 {
-    font-size: 18px;
-    max-width: 40%;
-    margin-bottom: 15px;
-  }
 `;
