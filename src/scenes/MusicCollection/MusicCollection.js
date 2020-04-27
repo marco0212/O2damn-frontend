2b4306002163eda686883493afeb8feda26a162a
Jeong <inyeop0212@gmail.com>
2020-04-27 12:08:16 +0900

    Render MusicCollection #1

--- src/scenes/MusicCollection/MusicCollection.js
+++ src/scenes/MusicCollection/MusicCollection.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import SceneHeader from '../../components/SceneHeader/SceneHeader';
-import SongList from '../../components/SongList/SongList';
-import SongInfo from '../../components/SongInfo/SongInfo';
 
 export default function MusicCollection({
   songs,
@@ -17,36 +14,74 @@ export default function MusicCollection({
     musicUrl: currentSongUrl
   } = currentSong;
   return (
-    <Container>
+    <Wrapper>
       <audio autoPlay src={currentSongUrl} />
-      <SceneHeader title='Select music' />
-      <Main>
-        <SongList
-          songs={songs}
-          currentSongId={currentSongId}
-          onSongItemClick={onSongItemClick}
-        />
-        <SongInfo
-          currentSongId={currentSongId}
-          currentSongTitle={currentSongTitle}
-          currentSongThumb={currentSongThumb}
-          onStartClick={onStartClick}
-        />
-      </Main>
-    </Container>
+      <BgOverlay style={{backgroundImage: `url(https://i2.wp.com/metro.co.uk/wp-content/uploads/2016/09/ad_165364642.jpg)`}}/>
+      <SongArea>
+        <SongList>
+          {
+            songs.map(song => {
+              const {title, artistName} = song;
+              return (
+                <SongItm>
+                    <h3>{title}</h3>
+                    <p>{artistName}</p>
+                </SongItm>
+              );
+            })
+          }
+        </SongList>
+      </SongArea>
+    </Wrapper>
   );
 }
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
+const Wrapper = styled.div`
   height: 100%;
+  position: relative;
 `;
-const Main = styled.main`
-  width: 100%;
-  max-width: 960px;
-  margin: 0 auto;
-  height: calc(100% - 100px);
-  display: flex;
-  padding-bottom: 30px;
+const BgOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: no-repeat center center / cover;
+  z-index: -1;
+
+  &:after {
+    content: '';
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, .3);
+  }
+`;
+const SongArea = styled.div`
+  float: right;
+  padding: 90px 30px;
+`;
+const SongList = styled.ul`
+  width: 600px;
+`;
+const SongItm = styled.li`
+  border-radius: 10px;
+  background-color: #fff;
+  padding: 10px 20px;
+  margin-bottom: 20px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+
+  & h3 {
+    font-size: 25px;
+    margin-bottom: 15px;
+  }
+
+  & p {
+    font-size: 20px;
+  }
 `;
