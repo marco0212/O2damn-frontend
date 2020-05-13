887e61f08d6805b3ff9a2333a0a5baa11155df30
Jeong <inyeop0212@gmail.com>
2020-05-13 12:36:34 +0900

    Update redux store
    Apply redux logger middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import logger from "redux-logger";
 import rootReducer from '../reducers';
 
-export default createStore(rootReducer, applyMiddleware(thunk));
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
+
+export default createStore(rootReducer, applyMiddleware(...middlewares));
